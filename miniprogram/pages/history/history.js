dcc91a6e7ad7f52365fc30d9cbf971abb476b5c2
norahliuu <norahspringliu@gmail.com>
2026-09-02 22:52:44 -0400

    ui

--- miniprogram/pages/history/history.js
+++ miniprogram/pages/history/history.js
@@ -22,5 +22,11 @@ Page({
     wx.setStorageSync('recipeHistory', history)
     this.setData({ historyList: history })
     wx.showToast({ title: '已删除', icon: 'success' })
+  },
+
+  onCreateRecipe() {
+    wx.navigateTo({
+      url: '/pages/edit/edit?mode=create'
+    })
   }
 })
